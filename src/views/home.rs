fb904c1b8eeb99939ed268d058b7ed32a3ce9fd8
Francesco Paoletti <paoletti.francesco@gmail.com>
2026-07-28 22:21:02 +0200

    feat(home): footer now gets pushed to the bottom by growing main section

--- src/views/home.rs
+++ src/views/home.rs
@@ -3,10 +3,22 @@ use dioxus::prelude::*;
 
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
-/// The Home page component that will be rendered when the current route is `[Route::Home]`
+// if CSS is needed, create a css file on below path and unconmment
+// both this line and the socument::Link line inside the body of
+// About component
+// const HOME_CSS: Asset = asset!("/assets/styling/about.css");
+
 #[component]
 pub fn Home() -> Element {
     rsx! {
+        // if CSS is needed, uncomment this line and the line outside
+        // this component
+        // document::Link { rel: "stylesheet", href: HOME_CSS }
+
+        main {
+            id: "home-page",
+            class: "grow",
+
         Hero {
             class: "bg-base-200",
             HeroContent {
@@ -28,5 +40,6 @@ pub fn Home() -> Element {
             }
         }
         Echo {}
+        }
     }
 }
