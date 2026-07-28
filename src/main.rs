b73df249450cbd73246b69fe55fa163ce725ff4c
Francesco Paoletti <paoletti.francesco@gmail.com>
2026-07-28 13:51:30 +0200

    feat(app): add utils module with helper functions for css classes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use dioxus::prelude::*;
 
 use views::{Blog, Home, Instructions, Layout};
 
+mod utils;
+
 /// Define a components module that contains all shared components for our app.
 mod components;
 /// Define a views module that contains the UI for all Layouts and Routes for our app.
